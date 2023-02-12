c9680618f691b67a5285a82e66a171effad8a10c
lahcen khouchane <90802568+lahcenkh@users.noreply.github.com>
2023-02-12 19:14:30 +0100

    Update get_net_ip_output_file.py

--- get_net_ip_output_file.py
+++ get_net_ip_output_file.py
@@ -1,13 +1,13 @@
 from ip_address_net_id import  Network_IP_Address_ID
 import csv
 
-file_db = "MSAN_HW_DB.csv"
-out_put_f = "msan_db_net.csv"
+file_db = "file_name.csv"
+out_put_f = "out_put_file.csv"
 
-with open(file_db, 'r') as nk_ipran_msan:
-    csv_reader = csv.reader(nk_ipran_msan)
-    with open(out_put_f, "w") as msan_nk_net:
+with open(file_db, 'r') as file_csv1:
+    csv_reader = csv.reader(file_csv1)
+    with open(out_put_f, "w") as file_csv2:
         
         for line in csv_reader:
             ip = Network_IP_Address_ID(line[1],line[2])
-            msan_nk_net.write(f"{line[0]},{ip},{line[1]},{line[2]},{line[3]}\n")
+            file_csv2.write(f"{line[0]},{ip},{line[1]},{line[2]},{line[3]}\n")
